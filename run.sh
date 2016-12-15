2e653ec58c673829849ac3482e07af9535afb946
Troy Harvey <troy.harvey@generationtux.com>
2016-12-15 15:31:25 -0500

    Bump version to 1.0.0

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@ then
     fail 'missing service endpoint to check, please add this to the validate version in wercker.yml'
 fi
 
+info "curling this url:"
 info "$WERCKER_VALIDATE_VERSION_URL?version=$WERCKER_GIT_COMMIT"
 
-curl "$WERCKER_VALIDATE_VERSION_URL?version=$WERCKER_GIT_COMMIT" --fail && success "container matches the current app version"
+curl "$WERCKER_VALIDATE_VERSION_URL?version=$WERCKER_GIT_COMMIT" --fail && \
+success "$WERCKER_VALIDATE_VERSION_URL is running:\n$WERCKER_GIT_COMMIT"
