230d0f18a8ca46b5e341230b5750fb6683f40943
Wes Eklund <wes.eklund@generationtux.com>
2017-01-12 11:31:48 -0500

    loop until replicas are done loading

--- run.sh
+++ run.sh
@@ -3,8 +3,15 @@ then
     fail 'missing service endpoint to check, please add this to the validate version in wercker.yml'
 fi
 
+
 info "curling this url:"
+
 info "$WERCKER_VALIDATE_VERSION_URL?version=$WERCKER_GIT_COMMIT"
 
-curl "$WERCKER_VALIDATE_VERSION_URL?version=$WERCKER_GIT_COMMIT" --silent --fail --show-error --output /dev/null && \
+while : ; do
+    EXIT_CODE = curl "$WERCKER_VALIDATE_VERSION_URL?version=$WERCKER_GIT_COMMIT" --silent --fail --show-error --output /dev/null && \
+    sleep .5
+
+    [[ $EXIT_CODE -ne 0 ]] || break
+done
 success "$WERCKER_VALIDATE_VERSION_URL is running:\n$WERCKER_GIT_COMMIT"
